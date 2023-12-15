85f6aa1eaf3accb2494924d30892ab7c842960a2
Inncrewin <aditya.kumar@inncrewin.com>
2023-12-16 01:26:28 +0530

    Initial Commit

---
+++ 3.DLL/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  3.DLL
+//
+//  Created by inncrewin on 16/12/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
